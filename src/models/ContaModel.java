bb0bd9cac537a8f937a2d5a91be54f2da2df1748
alvaro-star <vargasalvaro248@gmail.com>
2022-12-18 11:25:47 -0400

    Versao final 2

--- src/models/ContaModel.java
+++ src/models/ContaModel.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
-/**
- *
- * @author 07884569116
- */
 public class ContaModel {
 
     //Chave estrangeira
--- src/services/ContaService.java
+++ src/services/ContaService.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package services;
 
 import dao.ConexaoBanco;
@@ -11,10 +7,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import models.ContaModel;
 
-/**
- *
- * @author alvaro
- */
 public class ContaService {
 
     public ArrayList<ContaModel> getAll() throws SQLException {
